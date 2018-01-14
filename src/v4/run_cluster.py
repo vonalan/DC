caba1721f409acbf1f5b55176c00cb02f2981e68
vonalan <kingdom_lee@outlook.com>
2018-01-14 22:03:05 +0800

    modified:   src/v4/run_cluster.py

--- src/v4/run_cluster.py
+++ src/v4/run_cluster.py
@@ -12,24 +12,29 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import minmax_scale as sklscale
 from sklearn.metrics import normalized_mutual_info_score as sklnmi
 
+split_round = 9
+database_name = 'ucf'
+database_root = r'D:\Users\kingdom\Datasets\UCF'
+num_classes = 10
 
 parser = argparse.ArgumentParser()
-parser.add_argument('database_name', type='str', default='ucf')
-parser.add_argument('split_round', type=int, default=9)
-parser.add_argument('--path_to_ctrain', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_ctrain_r9.txt')
-parser.add_argument('--path_to_xtrain', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_xtrain_r9.txt')
-parser.add_argument('--path_to_ytrain', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_ytrain_r9.txt')
-parser.add_argument('--path_to_ctest', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_ctest_r9.txt')
-parser.add_argument('--path_to_xtest', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_xtest_r9.txt')
-parser.add_argument('--path_to_ytest', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_ytest_r9.txt')
-parser.add_argument('--path_to_xrand', type=str, default=r'D:\Users\kingdom\GIT\DC_OLD\data\kth_xrand_r9.txt')
+parser.add_argument('--database_name', type=str, default=database_name)
+parser.add_argument('--split_round', type=int, default=split_round)
+parser.add_argument('--path_to_ctrain', type=str, default=os.path.join(database_root, 'data\pwd\%s_ctrain_r9.txt'%(database_name)))
+parser.add_argument('--path_to_xtrain', type=str, default=os.path.join(database_root, 'data\pwd\%s_xtrain_r9.txt'%(database_name)))
+parser.add_argument('--path_to_ytrain', type=str, default=os.path.join(database_root, 'data\pwd\%s_ytrain_r9.txt'%(database_name)))
+parser.add_argument('--path_to_ctest', type=str, default=os.path.join(database_root, 'data\pwd\%s_ctest_r9.txt'%(database_name)))
+parser.add_argument('--path_to_xtest', type=str, default=os.path.join(database_root, 'data\pwd\%s_xtest_r9.txt'%(database_name)))
+parser.add_argument('--path_to_ytest', type=str, default=os.path.join(database_root, 'data\pwd\%s_ytest_r9.txt'%(database_name)))
+parser.add_argument('--path_to_xrand', type=str, default=os.path.join(database_root, 'data\pwd\%s_xrand_r9.txt'%(database_name)))
 parser.add_argument('--depict_output_dim', type=int, default=-1)
 parser.add_argument('--rbfnn_input_dim', type=int, default=-1)
 parser.add_argument('--rbfnn_num_center', type=int, default=-1)
-parser.add_argument('--rbfnn_output_dim', type=int, default=-1)
+parser.add_argument('--rbfnn_output_dim', type=int, default=num_classes)
 parser.add_argument('--save_model_dir', type=str, default='../../models/')
 parser.add_argument('--save_results_dir', type=str, default='../../results/')
 FLAGS, _ = parser.parse_known_args()
+pprint.pprint(FLAGS)
 
 
 import cluster
@@ -38,10 +43,12 @@ import classifier
 
 xtrain = np.loadtxt(FLAGS.path_to_xtrain)
 xtest = np.loadtxt(FLAGS.path_to_xtest)
+print(xtrain.shape, xtest.shape)
 
-
+FLAGS.rbfnn_num_center = 120
 for i in range(7, 16 + 1):
     num_cluster = 1 << i
+    print(num_cluster)
     FLAGS.depict_output_dim = num_cluster
     FLAGS.rbfnn_input_dim = num_cluster
     kms = cluster.build_kmeans_model_with_fixed_inputs(FLAGS, num_cluster)
